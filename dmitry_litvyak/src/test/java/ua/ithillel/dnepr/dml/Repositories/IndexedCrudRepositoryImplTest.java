93918927e3110ae325c0736751fe05d56ae8b252
MrAoen <dmitry.litvyak@gmail.com>
2020-01-03 11:42:36 +0300

    правки согласно замечаний наименованиея полей вынесены в константы)

---
+++ dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/IndexedCrudRepositoryImplTest.java
@@ -0,0 +1,53 @@
+package ua.ithillel.dnepr.dml.Repositories;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexedCrudRepositoryImplTest {
+
+    private String rootDirectory;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByField() {
+    }
+
+    @Test
+    void addIndex() {
+    }
+
+    @Test
+    void addIndexes() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
