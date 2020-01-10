b5d128a177c409b615702dc17e34bc289833ed2e
helloUsername <dmitry.litvyak@gmail.com>
2020-01-10 16:24:18 +0200

    полностью реализован репозиторий с Н2. все покрыто тестами

--- dmitry_litvyak/src/main/java/ua/ithillel/dnepr/dml/Repositories/jdbcCrudRepositoryImpl.java
+++ dmitry_litvyak/src/main/java/ua/ithillel/dnepr/dml/Repositories/jdbcCrudRepositoryImpl.java
@@ -1,5 +1,6 @@
 package ua.ithillel.dnepr.dml.Repositories;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import lombok.extern.slf4j.Slf4j;
 import ua.ithillel.dnepr.common.repository.IndexedCrudRepository;
 import ua.ithillel.dnepr.common.repository.entity.AbstractEntity;
@@ -29,72 +30,172 @@ public class jdbcCrudRepositoryImpl<EntityType extends AbstractEntity<IdType>, I
 
     @Override
     public Optional<List<EntityType>> findAll() {
-        final List<EntityType> result = new ArrayList<>();
-        String entityName = clazz.getSimpleName();
+        String findAllQuery = "select * from public." + clazz.getSimpleName();
+        return execListQuery(findAllQuery);
+    }
+
+    private Optional<List<EntityType>> execListQuery(String queryString){
+        Optional<List<EntityType>> result = Optional.empty();
+        final List<EntityType> entityList = new ArrayList<>();
         try (final Statement stmt = connection.createStatement()) {
-            ResultSet resultSet = stmt.executeQuery("select * from public." + entityName);
+            ResultSet resultSet = stmt.executeQuery(queryString);
             while (resultSet.next()) {
-                EntityType entity = clazz.getDeclaredConstructor().newInstance();
-                Stream<Method> methodStream = Stream.of(clazz.getDeclaredMethods());
-                methodStream.filter(method->method.getName().startsWith("set")).forEach(method -> {
-                    Object value = null;
-                    try {
-                        value = resultSet.getObject(method.getName().replaceFirst("set",""));
-                    } catch (SQLException e) {
-                        log.error("SQL error while get field value",e);
-                    }
-                    Class<?>[] types = method.getParameterTypes();
-                    if(types.length>0){
-                        //types[0].cast()
-                        try {
-                            method.invoke(entity,types[0].cast(value));
-                        } catch (IllegalAccessException | InvocationTargetException e) {
-                           log.error("Unecpected filed",e);
-                        }
-                    }
-                });
-                result.add(entity);
+                EntityType entity = getEntityFromDBResulSet(resultSet);
+                entityList.add(entity);
+            }
+            if(!entityList.isEmpty()){
+                result = Optional.of(entityList);
             }
         } catch (SQLException e) {
             log.error("Failed to find all elements", e);
         } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
             log.error("wrong POJO",e);
         }
-        return Optional.of(result);
+        return result;
+    }
+
+    private EntityType getEntityFromDBResulSet(ResultSet resultSet) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        EntityType entity = clazz.getDeclaredConstructor().newInstance();
+        Stream<Method> methodStream = Stream.of(clazz.getDeclaredMethods());
+        methodStream.filter(method->method.getName().startsWith("set")).forEach(method -> {
+            Object value = null;
+            try {
+                value = resultSet.getObject(method.getName().replaceFirst("set",""));
+            } catch (SQLException e) {
+                log.error("SQL error while get field value",e);
+            }
+            Class<?>[] types = method.getParameterTypes();
+            if(types.length>0){
+                try {
+                    method.invoke(entity,types[0].cast(value));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                   log.error("Unecpected filed",e);
+                }
+            }
+        });
+        return entity;
     }
 
     @Override
     public Optional<EntityType> findById(IdType id) {
-        return Optional.empty();
+        Optional<EntityType> result =  Optional.empty();
+        String entityName = clazz.getSimpleName();
+        try (final Statement stmt = connection.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery("select * from public." + entityName+" WHERE Id="+id);
+            if(resultSet.next()) {
+                result = Optional.of(getEntityFromDBResulSet(resultSet));
+            }
+        }catch (Exception e){
+            log.error("Failed to find element by Id",e);
+        }
+        return result;
     }
 
     @Override
     public Optional<List<EntityType>> findByField(String fieldName, Object value) {
-        return Optional.empty();
+        StringBuilder findByFieldQuery = new StringBuilder();
+        findByFieldQuery.append("select * from public.").append(clazz.getSimpleName()).append(" WHERE ").append(fieldName).append("=\'").append(value).append("\'");
+        return execListQuery(findByFieldQuery.toString());
     }
 
     @Override
     public void addIndex(String field) {
-
+        List<String> fields = new ArrayList<>();
+        fields.add(field);
+        addIndexes(fields);
     }
 
     @Override
     public void addIndexes(List<String> fields) {
-
+        StringBuilder query = new StringBuilder();
+        for (String field:fields) {
+            query.append(" CREATE INDEX idx_").append(field).append(" ON ")
+                    .append(clazz.getSimpleName())
+                    .append('(').append(field).append(')')
+                    .append(';');
+        }
+        try(Statement stmt = connection.createStatement()){
+            stmt.execute(query.toString());
+        }catch (Exception e){
+            log.error("Add index failure",e);
+        }
     }
 
     @Override
     public EntityType create(EntityType entity) {
-        return null;
+        try (final Statement stmt = connection.createStatement()) {
+            StringBuilder insertQueryStart = new StringBuilder("INSERT INTO " + clazz.getSimpleName() + " (");
+            StringBuilder insertQueryEnd = new StringBuilder(" VALUES ( ");
+            Stream<Method> methodStream = Stream.of(clazz.getDeclaredMethods());
+            methodStream.filter(method -> method.getName().startsWith("get")).forEach(method -> {
+                String fieldName;
+                fieldName = method.getName().replaceFirst("get", "");
+                if (insertQueryStart.indexOf(fieldName) == -1) {
+                    insertQueryStart.append(fieldName).append(',');
+                    try {
+                        insertQueryEnd.append('\'').append(method.invoke(entity).toString()).append('\'').append(',');
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        log.error("Create invoke problem", e);
+                    }
+                }
+            });
+            insertQueryEnd.deleteCharAt(insertQueryEnd.length() - 1).append(')');
+            insertQueryStart.deleteCharAt(insertQueryStart.length() - 1).append(')').append(insertQueryEnd.toString());
+            stmt.execute(insertQueryStart.toString());
+        } catch (Exception e) {
+            log.error("Create operation failed", e);
+        }
+        return entity;
     }
 
     @Override
     public EntityType update(EntityType entity) {
-        return null;
+        Optional<EntityType> DBentity = findById(entity.getId());
+        if(DBentity.isPresent()){
+            StringBuilder query = new StringBuilder("UPDATE ");
+            query.append(clazz.getSimpleName()).append(" SET ");
+            Stream<Method> methodStream = Stream.of(clazz.getDeclaredMethods());
+            methodStream.filter(method -> method.getName().startsWith("get")).forEach(method -> {
+                String fieldName;
+                fieldName = method.getName().replaceFirst("get", "");
+                if (query.indexOf(fieldName) == -1) {
+                    query.append(fieldName).append('=');
+                    try {
+                        query.append('\'').append(method.invoke(entity).toString()).append('\'').append(',');
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        log.error("Create invoke problem", e);
+                    }
+                }
+            });
+            query.deleteCharAt(query.length() - 1).append(" WHERE ID=").append(entity.getId());
+            try (final Statement stmt = connection.createStatement()) {
+                stmt.execute(query.toString());
+            }catch (Exception e){
+                log.error("Update failed",e);
+            }
+        }else {
+            create(entity);
+        }
+        return entity;
     }
 
     @Override
     public EntityType delete(IdType id) {
-        return null;
+        Optional<EntityType> entity = findById(id);
+        if(entity.isPresent()){
+            try (final Statement stmt = connection.createStatement()) {
+                stmt.execute("DELETE FROM "+clazz.getSimpleName()+ " WHERE ID="+id);
+            }catch (Exception e){
+                log.error("DELETE failed",e);
+            }
+        }
+        if(entity.isEmpty()){
+            try {
+                entity = Optional.of(clazz.getDeclaredConstructor().newInstance());
+            }catch (Exception e){
+                log.error("New Entity",e);
+            }
+        }
+        return entity.get();
     }
 }
--- dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/jdbcCrudRepositoryTest.java
+++ dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/jdbcCrudRepositoryTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.TestInstance;
 import ua.ithillel.dnepr.common.utils.H2Server;
 import ua.ithillel.dnepr.common.utils.NetUtils;
 import ua.ithillel.dnepr.dml.domain.City;
+import ua.ithillel.dnepr.dml.service.LoggerTrigger;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +23,8 @@ class jdbcCrudRepositoryTest {
     private jdbcCrudRepositoryImpl testRepository;
     H2Server dbserver;
     private Connection conn;
+    private City tmpCity;
+    private LoggerTrigger trigger;
 
     @BeforeAll
     void generalSetup() throws SQLException, ClassNotFoundException {
@@ -38,7 +41,16 @@ class jdbcCrudRepositoryTest {
     @BeforeEach
     void setUp() throws SQLException{
         conn = DriverManager.getConnection("jdbc:h2:file:./src/main/resources/h2crudbase","sa","");
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TRIGGER IF EXISTS CITY_INSERT; CREATE TRIGGER CITY_INSERT AFTER UPDATE ON CITY FOR EACH ROW CALL \"ua.ithillel.dnepr.dml.service.LoggerTrigger\" ");
+
         testRepository = new jdbcCrudRepositoryImpl(conn, City.class);
+        tmpCity = new City();
+        tmpCity.setName("Bandershtadt");
+        tmpCity.setRegion_id(1000);
+        tmpCity.setId(99999);
+        tmpCity.setCountry_id(1000);
     }
 
     @Test
@@ -48,14 +60,18 @@ class jdbcCrudRepositoryTest {
 
     @Test
     void findById() {
+        assertNotNull(testRepository.findById(9999));
     }
 
     @Test
     void findByField() {
+        assertNotNull(testRepository.findByField("Name","Абрамцево"));
+        assertTrue(testRepository.findByField("Name","Нью Васюки").isEmpty());
     }
 
     @Test
     void addIndex() {
+        testRepository.addIndex("name");
     }
 
     @Test
@@ -64,13 +80,20 @@ class jdbcCrudRepositoryTest {
 
     @Test
     void create() {
+
+        assertEquals(testRepository.create(tmpCity),tmpCity);
+        testRepository.delete(tmpCity.getId());
     }
 
     @Test
     void update() {
+        tmpCity.setName("Lviv1715");
+        assertNotEquals( ((City)testRepository.update(tmpCity)).getName(),"Bandershtadt");
     }
 
     @Test
     void delete() {
+        testRepository.update(tmpCity);
+        assertEquals(testRepository.delete(99999).getId(),99999);
     }
 }
