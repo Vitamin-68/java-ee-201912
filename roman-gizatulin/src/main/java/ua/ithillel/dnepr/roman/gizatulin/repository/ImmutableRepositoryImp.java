85c4d1a8b624d75cf6b2480170d339f632953656
hitower2000 <hitower2000@gmail.com>
2020-01-14 10:43:27 +0200

    add crudRepo

--- roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/repository/ImmutableRepositoryImp.java
+++ roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/repository/ImmutableRepositoryImp.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
+
 @Slf4j
 public class ImmutableRepositoryImp<EntityType extends AbstractEntity<IdType>, IdType>
         extends BasePrefixFileRepository<EntityType, IdType>
