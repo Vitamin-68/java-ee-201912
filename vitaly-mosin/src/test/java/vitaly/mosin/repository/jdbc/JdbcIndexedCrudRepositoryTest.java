34d1663b8db4a86c104b9f4041fd205b3386f52c
Vitamin_68 <wetalm68@gmail.com>
2020-01-18 13:42:08 +0200

    исправил путь к временным csv кодировку пока нет

---
+++ vitaly-mosin/src/test/java/vitaly/mosin/repository/jdbc/JdbcIndexedCrudRepositoryTest.java
@@ -0,0 +1,50 @@
+package vitaly.mosin.repository.jdbc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcIndexedCrudRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByField() {
+    }
+
+    @Test
+    void addIndex() {
+    }
+
+    @Test
+    void addIndexes() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
