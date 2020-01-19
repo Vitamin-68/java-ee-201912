ae2f7ae3c9b086fd6b62859fa79c884e568d0eef
unsreg <unsreg@gmail.com>
2020-01-19 16:37:24 +0200

    H2Server refactored

--- common-integration-test/src/test/java/ua/ithillel/dnepr/common/test/repository/roman/gizatulin/JdbcIndexedCrudRepositoryIntegrationTest.java
+++ common-integration-test/src/test/java/ua/ithillel/dnepr/common/test/repository/roman/gizatulin/JdbcIndexedCrudRepositoryIntegrationTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ua.ithillel.dnepr.common.test.repository.TestEntity;
 import ua.ithillel.dnepr.common.utils.H2Server;
-import ua.ithillel.dnepr.common.utils.NetUtils;
 import ua.ithillel.dnepr.roman.gizatulin.repository.JdbcIndexedCrudRepository;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static ua.ithillel.dnepr.common.test.repository.CrudRepositoryIntegrationTestHelper.testCreateManyNewEntities;
@@ -21,30 +19,21 @@ import static ua.ithillel.dnepr.common.test.repository.CrudRepositoryIntegration
 
 @Slf4j
 class JdbcIndexedCrudRepositoryIntegrationTest {
-    private static final int PORT = NetUtils.getFreePort();
-    private static final String TEST_DB_NAME = "test_db.sqlite3";
+    private static final String TEST_DB_NAME = "test_db";
 
     private H2Server h2Server;
     private JdbcIndexedCrudRepository<TestEntity, Integer> crudRepository;
 
     @BeforeEach
-    void setup() throws IOException, SQLException, ClassNotFoundException {
+    void setup() throws IOException, SQLException {
         String repoRootPath = Path.of(
                 Files.createTempDirectory("romanGizatulin").toString(),
                 TEST_DB_NAME
         ).toString();
-        h2Server = new H2Server(PORT);
-        h2Server.start();
+        h2Server = new H2Server();
 
-        Class.forName("org.h2.Driver");
-        final String connectionString = String.format(
-                "jdbc:h2:tcp://%s:%s/%s",
-                NetUtils.getHostName(),
-                PORT,
-                repoRootPath
-        );
-        log.info("Database connection string: {}", connectionString);
-        Connection connection = DriverManager.getConnection(connectionString);
+        log.info("Database path: {}", repoRootPath);
+        Connection connection = h2Server.getConnection(repoRootPath);
         crudRepository = new JdbcIndexedCrudRepository<>(connection, TestEntity.class);
     }
 
