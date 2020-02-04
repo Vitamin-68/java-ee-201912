e79322f23cfb32a3b87ef43439a528c26422aace
helloUsername <dmitry.litvyak@gmail.com>
2020-02-04 17:22:28 +0200

    настройка liquibase

---
+++ dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/JpaCrudRepositoryTest.java
@@ -0,0 +1,43 @@
+package ua.ithillel.dnepr.dml.Repositories;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class JpaCrudRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByField() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void close() {
+    }
+}
