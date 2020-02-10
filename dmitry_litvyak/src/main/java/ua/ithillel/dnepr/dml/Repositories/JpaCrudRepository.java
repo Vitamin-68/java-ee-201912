87faf11f34130dd15cf80eb0cd38f46c35a0e3b5
helloUsername <dmitry.litvyak@gmail.com>
2020-02-10 16:18:58 +0200

    create неработает даже на несвязанном классе

--- dmitry_litvyak/src/main/java/ua/ithillel/dnepr/dml/Repositories/JpaCrudRepository.java
+++ dmitry_litvyak/src/main/java/ua/ithillel/dnepr/dml/Repositories/JpaCrudRepository.java
@@ -1,7 +1,6 @@
 package ua.ithillel.dnepr.dml.Repositories;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
 import ua.ithillel.dnepr.common.repository.ImmutableRepository;
 import ua.ithillel.dnepr.common.repository.MutableRepository;
 import ua.ithillel.dnepr.common.repository.entity.AbstractEntity;
--- dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/JpaCrudRepositoryTest.java
+++ dmitry_litvyak/src/test/java/ua/ithillel/dnepr/dml/Repositories/JpaCrudRepositoryTest.java
@@ -1,22 +1,16 @@
 package ua.ithillel.dnepr.dml.Repositories;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ua.ithillel.dnepr.dml.domain.jpa.City;
-import ua.ithillel.dnepr.dml.domain.jpa.Country;
-import ua.ithillel.dnepr.dml.domain.jpa.Region;
 import ua.ithillel.dnepr.dml.domain.jpa.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.EntityTransaction;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,42 +18,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Slf4j
 class JpaCrudRepositoryTest {
 
-    private static SessionFactory sessionFactory;
-    private final String DATABASE_NAME = System.getProperty("user.dir") + "\\target\\classes\\dev\\db\\main";
     private JpaCrudRepository jpaCrudRepository;
     private Class testClass = City.class;
     private EntityManager entityManager;
 
     public JpaCrudRepositoryTest() {
-        setupsession();
-    }
 
-    private void setupsession() {
-
-        if (sessionFactory == null) {
-            Configuration config = new Configuration();
-            Properties settings = new Properties();
-            settings.put(Environment.URL, "jdbc:h2:file:\\" + DATABASE_NAME);
-            settings.put(Environment.USER, "sa");
-            settings.put(Environment.PASS, "");
-            settings.put(Environment.DRIVER,"org.h2.Driver");
-            settings.put(Environment.SHOW_SQL,true);
-            settings.put(Environment.HBM2DDL_AUTO,"validate");
-            settings.put(Environment.DIALECT,"org.hibernate.dialect.H2Dialect");
-            //settings.put(Environment.)
-            config.setProperties(settings);
-            config.addAnnotatedClass(testClass);
-            config.addAnnotatedClass(Country.class);
-            config.addAnnotatedClass(Region.class);
-            config.addAnnotatedClass(User.class);
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-            sessionFactory = config.buildSessionFactory(serviceRegistry);
-        }
-        try {
-            entityManager = sessionFactory.createEntityManager();
-        } catch (Exception e) {
-            log.error("Unable init entity manager:", e);
-        }
+        entityManager = Persistence.createEntityManagerFactory("persistence-unit-dml").createEntityManager();
     }
 
     @BeforeEach
@@ -92,7 +57,8 @@ class JpaCrudRepositoryTest {
         user.setId(1);
         user.setFName("Jhon");
         user.setLName("Dow");
-        userRepo.create(user);
+        jpaCrudRepository.create(user);
+        //userRepo.create(user);
 //        Integer lId = 99998;
 //        City tmpCity = new City();
 //        tmpCity.setName("Bandershtadt");
