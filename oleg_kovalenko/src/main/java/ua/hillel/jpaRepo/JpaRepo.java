35d34405639cf461601e07f9eababd28b56b2385
oleg <oleg.kovalenko.workmail@gmail.com>
2020-02-18 20:40:03 +0200

    Убрал лишнее

--- oleg_kovalenko/src/main/java/ua/hillel/jpaRepo/JpaRepo.java
+++ oleg_kovalenko/src/main/java/ua/hillel/jpaRepo/JpaRepo.java
@@ -3,16 +3,11 @@ package ua.hillel.jpaRepo;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import ua.hillel.jpa_entity.CityJpa;
-import ua.hillel.jpa_entity.CountryJpa;
-import ua.hillel.jpa_entity.RegionJpa;
 import ua.ithillel.dnepr.common.repository.CrudRepository;
 import ua.ithillel.dnepr.common.repository.entity.AbstractEntity;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
