617a111e0f7f0c853315060fd6940eddbb8db8d6
Roman Gizatulin <unsreg@gmail.com>
2020-02-22 17:16:12 +0200

    Code refactoring

--- roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/rmi/ProgramServer.java
+++ roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/rmi/ProgramServer.java
@@ -1,37 +1,20 @@
 package ua.ithillel.dnepr.roman.gizatulin.rmi;
 
 import lombok.extern.slf4j.Slf4j;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
+import ua.ithillel.dnepr.common.utils.NetUtils;
 
 @Slf4j
 public class ProgramServer {
-    private static final int RMI_PORT = 5000;
-    private static final String RMI_HOST = "localhost";
-
-    public interface Test extends Remote {
-        String getString(String test) throws RemoteException;
-    }
-
-    public static class TestImpl extends UnicastRemoteObject implements Test {
-        protected TestImpl() throws RemoteException {
-            super();
-        }
-
-        @Override
-        public String getString(String test) {
-            return test + "_result";
-        }
-    }
+    private static final int RMI_PORT = NetUtils.getFreePort();
+    private static final String RMI_HOST = NetUtils.getHostName();
+    private static final String OBJECT_NAME = CalcRemote.class.getSimpleName();
 
     public static void main(String[] args) throws Exception {
         CalcRemote test = new CalcImpl();
 
-        RmiUtils.startServer(RMI_PORT, RMI_HOST, test);
+        RmiUtils.startServer(RMI_PORT, RMI_HOST, OBJECT_NAME, test);
 
-        CalcRemote stub = RmiUtils.startClient(RMI_PORT, RMI_HOST, test.getClass());
+        CalcRemote stub = RmiUtils.startClient(RMI_PORT, RMI_HOST, OBJECT_NAME);
         for (int i = 1; i < 50; i++) {
             log.info(String.valueOf(stub.add(i + 34, i + 4)));
             log.info(String.valueOf(stub.subtract(i + 34, i + 4)));
--- roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/rmi/RmiUtils.java
+++ roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/rmi/RmiUtils.java
@@ -9,11 +9,15 @@ import java.rmi.registry.LocateRegistry;
 @UtilityClass
 public class RmiUtils {
     public static void startServer(int port, String host, Remote remoteObject) throws Exception {
+        startServer(port, host, remoteObject.getClass().getSimpleName(), remoteObject);
+    }
+
+    public static void startServer(int port, String host, String remotePath, Remote remoteObject) throws Exception {
         String rmiUri = String.format(
                 "rmi://%s:%s/%s",
                 host,
                 port,
-                remoteObject.getClass().getSimpleName());
+                remotePath);
 
         LocateRegistry.createRegistry(port);
         Naming.rebind(rmiUri, remoteObject);
@@ -21,11 +25,16 @@ public class RmiUtils {
 
     @SuppressWarnings("unchecked")
     public <T extends Remote> T startClient(int port, String host, Class<T> remoteObject) throws Exception {
+        return startClient(port, host, remoteObject.getSimpleName());
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Remote> T startClient(int port, String host, String remoteObject) throws Exception {
         String rmiUri = String.format(
                 "rmi://%s:%s/%s",
                 host,
                 port,
-                remoteObject.getSimpleName());
+                remoteObject);
         // lookup method to find reference of remote object
         return (T) Naming.lookup(rmiUri);
     }
