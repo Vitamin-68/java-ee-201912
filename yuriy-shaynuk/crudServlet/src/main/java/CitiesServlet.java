3eeebaf9e1fb4a3cc83434d27cf2d8ec23348948
hitower2000 <hitower2000@gmail.com>
2020-03-11 17:25:21 +0200

    cities servlet doGet

--- yuriy-shaynuk/crudServlet/src/main/java/CitiesServlet.java
+++ yuriy-shaynuk/crudServlet/src/main/java/CitiesServlet.java
@@ -1,23 +1,41 @@
+import ua.ithillel.dnepr.common.repository.CrudRepository;
+import ua.ithillel.dnepr.yuriy.shaynuk.repository.csv.CrudRepositoryImp;
+import ua.ithillel.dnepr.yuriy.shaynuk.repository.entity.City;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.stream.events.Characters;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @WebServlet("/cities/*")
 public class CitiesServlet extends HttpServlet {
-//    static final H2Server h2Server = new H2Server(12345);
-//    static private EntityManagerFactory entityManagerFactory;
-//    static private SpringCityRepositoryImp cityRepository;
-//    static private SpringRegionRepositoryImp regionRepository;
+    static File dataFile =null;
+    static private CrudRepository<City, Integer> cityRepository;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<h3>Helloooo World!</h3>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String id;
+        Optional<City> city = Optional.empty();
+        if(!request.getRequestURI().equals("/cities")) {
+            id = request.getRequestURI().replace("/cities/", "");
+            city = cityRepository.findById(Integer.valueOf(id));
+        }
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        PrintWriter out = response.getWriter();
+        if(city.isPresent()){
+            out.println(city.get().getName());
+        }else {
+            out.println("<h3>Helloooo World!</h3>");
+        }
     }
 
     @Override
@@ -42,12 +60,11 @@ public class CitiesServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
+        dataFile = Utils.createTempFile(getClass(),"city.csv");
 
-//        final AnnotationConfigApplicationContext annotationConfigApplicationContext =
-//                new AnnotationConfigApplicationContext(AppConfig.class);
-//        entityManagerFactory = annotationConfigApplicationContext.getBean(EntityManagerFactory.class);
-//        cityRepository = annotationConfigApplicationContext.getBean(SpringCityRepositoryImp.class);
-//        regionRepository = annotationConfigApplicationContext.getBean(SpringRegionRepositoryImp.class);
+        if (dataFile != null) {
+            cityRepository = new CrudRepositoryImp<>(dataFile.getPath(),City.class);
+        }
+        super.init();
     }
 }
---
+++ yuriy-shaynuk/crudServlet/src/main/java/Utils.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+    public static File createTempFile(Class clazz, String resourcePath) {
+        try {
+            File tempFile = File.createTempFile("tmp", resourcePath);
+            InputStream in = clazz.getClassLoader().getResourceAsStream(resourcePath);
+            if (in != null) {
+                try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                    //copy stream
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+            //log.debug("tmp file path: "+tempFile.getAbsolutePath());
+            return tempFile;
+        } catch (IOException e) {
+            //log.error("getResourceAsFile exception",e);
+            return null;
+        }
+    }
+}
