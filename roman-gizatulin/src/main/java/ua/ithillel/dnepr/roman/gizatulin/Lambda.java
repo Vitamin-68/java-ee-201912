39f34007caa814eb6a7ca63114b757b51dcbe67b
Roman Gizatulin <unsreg@gmail.com>
2020-03-14 16:18:10 +0200

    Added plantuml client-server diagram

---
+++ roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/Lambda.java
@@ -0,0 +1,10 @@
+package ua.ithillel.dnepr.roman.gizatulin;
+
+public class Lambda {
+    public static Object main(Object args) {
+        Object result = null;
+        //TODO: ...
+
+        return result;
+    }
+}
