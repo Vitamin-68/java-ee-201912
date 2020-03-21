2a3ff566bbe2e8f2ecf52b83ddfffb2abce24e3f
unsreg <unsreg@gmail.com>
2020-03-21 15:28:16 +0200

    Added cache loader

--- roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/jcache/Program00.java
+++ roman-gizatulin/src/main/java/ua/ithillel/dnepr/roman/gizatulin/jcache/Program00.java
@@ -7,7 +7,12 @@ import javax.cache.configuration.Configuration;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
+import javax.cache.integration.CacheLoader;
+import javax.cache.integration.CacheLoaderException;
 import javax.cache.spi.CachingProvider;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Program00 {
     public static <KEY, VALUE> Cache<KEY, VALUE> createCache(String cacheName) {
@@ -17,6 +22,19 @@ public class Program00 {
         final CacheManager cacheManager = cachingProvider.getCacheManager();
         final Configuration<KEY, VALUE> config = new MutableConfiguration<KEY, VALUE>()
                 .setStoreByValue(false)
+                .setCacheLoaderFactory(() -> new CacheLoader<>() {
+                    @Override
+                    public VALUE load(KEY key) throws CacheLoaderException {
+                        return loadAll(Collections.singletonList(key)).get(key);
+                    }
+
+                    @Override
+                    public Map<KEY, VALUE> loadAll(Iterable<? extends KEY> keys) throws CacheLoaderException {
+                        Map<KEY, VALUE> result = new HashMap<>();
+                        //TODO: get and return data from database
+                        return result;
+                    }
+                })
                 .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_MINUTE));
         return cacheManager.createCache(cacheName, config);
     }
