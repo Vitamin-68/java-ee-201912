cf8dd5301085ae9580c627f114dbb2dbfd6fe437
helloUsername <dmitry.litvyak@gmail.com>
2020-04-01 17:20:39 +0300

    REST + Cache на апдейт повешен евикт на весь кэш

---
+++ dmitry_litvyak/homework-13/src/test/java/ua/ithillel/dnepr/dml/rest/homework13/Homework13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ithillel.dnepr.dml.rest.homework13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
